39ebb9b9c96c64eee82e925fc9c9948d3933053c
krishane <shaner261996@gmail.com>
2017-02-13 16:09:00 -0600

    Added the Portfolio class to the Class of Libraries

---
+++ Ticker501-Class Library/Ticker501-Class Library/Portfolio.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ticker501_Class_Library
+{
+    public class Portfolio
+    {
+        List<Stock> _stock = new List<Stock>();
+        int quantity;
+        double _totalPositions = 0;
+        string _name;
+
+        /// <summary>
+        /// Adds a new portfolio
+        /// </summary>
+        /// <param name="name"></param>
+        public void AddPortfolio(string name)
+        {
+            _name = name;
+        }
+
+        /// <summary>
+        /// Adds stocks to the portfolio
+        /// </summary>
+        /// <param name="stock">The stock</param>
+        /// <param name="quantity">The quantity purchased</param>
+        public void AddStocks(Stock stock, int quantity)
+        {
+            _stock.Add(stock);
+            _totalPositions = stock.Price * quantity;
+            
+
+        }
+
+        /// <summary>
+        /// This property returns the name of said portfolio.
+        /// </summary>
+        public string PortfolioName
+        {
+            get
+            {
+                return _name;
+            }
+        }
+
+        /// <summary>
+        /// This method returns a list of stocks in the portfolio
+        /// </summary>
+        /// <returns>List of stocks</returns>
+        public List<Stock> GetStocks()
+        {
+            return _stock;
+        }
+
+        public Stock GetStocK(string ticker)
+        {
+            for(int i = 0; i < _stock.Count; i++)
+            {
+                if(ticker == _stock[i].Ticker)
+                {
+                    return _stock[i];
+                }
+            }
+
+            return null;
+
+        }
+
+        /// <summary>
+        /// Returns the updated stock prices in portfolio
+        /// </summary>
+        /// <param name="percentage"></param>
+        public void UpdateStocks(double percentage)
+        {
+            for (int i = 0; i < _stock.Count; i++)
+            {
+                double newPrice = _stock[i].Price * percentage;
+                _stock[i].Price += newPrice;
+
+
+            }
+
+            _totalPositions += (_totalPositions * percentage);
+
+
+
+        }
+
+
+
+    }
+}
---
+++ Ticker501-Class Library/Ticker501-Class Library/Stock.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ticker501_Class_Library
+{
+    public class Stock
+    {
+        private string _ticker;
+        private string _name;
+        private double _price;
+        
+        /// <summary>
+        /// This constuctor initializes the fields for the stock
+        /// </summary>
+        /// <param name="ticker">The ticker information</param>
+        /// <param name="name">The name of the company</param>
+        /// <param name="price">The price of the stock.</param>
+        public Stock(string ticker, string name, double price)
+        {
+            _ticker = ticker;
+            _name = name;
+            _price = price;
+
+        }
+        
+        /// <summary>
+        /// This property returns the Ticker ID of the stock.
+        /// </summary>
+        public string Ticker
+        {
+            get
+            {
+                return _ticker;
+            }
+        }
+
+        /// <summary>
+        /// This property returns the Company name of the stock.
+        /// </summary>
+        public string Company
+        {
+            get
+            {
+                return _name;
+            }
+        }
+
+        /// <summary>
+        /// This property returns the current Price of the stock.
+        /// </summary>
+        public double Price
+        {
+            get
+            {
+                return _price;
+            }set
+            {
+                _price = value;
+            }
+        }
+
+
+
+    }
+}
